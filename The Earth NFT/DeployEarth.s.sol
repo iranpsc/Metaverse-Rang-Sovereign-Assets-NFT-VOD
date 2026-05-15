f52ff32abea6a804539c52b916651e40caf5b13e
Ali Nasirlou <145068719+alinasirlou2020@users.noreply.github.com>
2026-05-15 11:09:14 +0330

    Add files via upload

---
+++ The Earth NFT/DeployEarth.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/TheEarth.sol";
+
+contract DeployEarth is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        TheEarth earth = new TheEarth();
+
+        vm.stopBroadcast();
+    }
+}
