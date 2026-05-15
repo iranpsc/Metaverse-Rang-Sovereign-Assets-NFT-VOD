ca1da8adf2c2d1261a53a85e22d852a14fbc83cb
Ali Nasirlou <145068719+alinasirlou2020@users.noreply.github.com>
2026-05-15 11:07:33 +0330

    Rename contract and update deployment logic

--- The Earth NFT/script/DeployAryana.s.sol
+++ The Earth NFT/script/DeployAryana.s.sol
@@ -2,28 +2,15 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import "../src/AryanaIsland.sol";
+import "../src/TheEarth.sol";
 
-contract DeployAryana is Script {
+contract DeployEarth is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address deployerAddress = vm.addr(deployerPrivateKey);
-
         vm.startBroadcast(deployerPrivateKey);
 
-        AryanaIsland island = new AryanaIsland(deployerAddress);
-
-        string memory islandURI =
-            "ipfs://bafkreihyowxudgomj6y5muwhmnv3iznn6yldccwc4cwn3wauscir5yu2um";
-
-        island.initializeIsland(deployerAddress, islandURI);
+        TheEarth earth = new TheEarth();
 
         vm.stopBroadcast();
-
-        console.log("-----------------------------------------");
-        console.log("Aryana Island Deployed at:", address(island));
-        console.log("Owner Address:", deployerAddress);
-        console.log("Symbol: ARYANA");
-        console.log("-----------------------------------------");
     }
 }
